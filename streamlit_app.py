becfae8b2356ee26c4b11d893dce1ae002ef0f42
kouki0404 <168718919+kouki0404@users.noreply.github.com>
2024-07-26 13:37:26 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,8 @@ yy = number[y]
 zz = number[z]
 nn = number[n]
 
-st.session_state.solution = 0
+if "solution" not in st.session_state:
+    st.session_state.solution = 0
 
 #出題範囲
 st.sidebar.title("章を選択してください")
@@ -33,32 +34,37 @@ fifth = ["制作中"]
 if chapter == "1章":
     serrect = st.sidebar.selectbox("出題範囲",first)
     if serrect == "合成速度":
-        st.write("速さ" + int[s] + ".0m/sで流れる川の中を、船(静水時での速さ" + ".0m/s)が川の流れと平行に進む")
+        st.write("速さ" + str(s) + ".0m/sで流れる川の中を、船(静水時での速さ" + ".0m/s)が川の流れと平行に進む")
         st.write("(1) 船が川下に向かって進む場合、川岸で静止している人から見た船の速度は何m/sか。(川下を正とする)")
         solution_one = st.text_area("(1)回答 ※数字のみ")
         st.write("(2) 船が川上に向かって進む場合、川岸で静止している人から見た船の速度は何m/sか。(川下を正とする)")
         solution_two = st.text_area("(2)回答 ※数字のみ")
-        st.write("(3) ①船が川を" + int[xx] + "m下るのと、②" + int[yy] + "m上るのにかかる時間はそれぞれ何秒か。")
+        st.write("(3) ①船が川を" + str(xx) + "m下るのと、②" + str(yy) + "m上るのにかかる時間はそれぞれ何秒か。")
         solution_three = st.text_area("(3)①回答 ※数字のみ")
         solution_four = st.text_area("(3)②回答 ※数字のみ")
 
         if st.button("解答確認"):
             st.session_state.solution += 1
-            if solution_one == s + f:
-                st.write("(1)正解")
-            else:
-                st.write("(1)不正解")
-            if solution_two == s - f:
-                st.write("(2)正解")
-            else:
-                st.write("(2)不正解")
-            if solution_three == xx / (s + f):
-                st.write("(3)①正解")
-            else:
-                st.write("(3)①不正解")
-            if solution_four == yy / (f - s):
-                st.write("(3)②正解")
-            else:
-                st.write("(3)②不正解")
+
+            try:
+                if solution_one == s + f:
+                    st.write("(1)正解")
+                else:
+                    st.write("(1)不正解")
+                if solution_two == s - f:
+                    st.write("(2)正解")
+                else:
+                    st.write("(2)不正解")
+                if solution_three == xx / (s + f):
+                    st.write("(3)①正解")
+                else:
+                    st.write("(3)①不正解")
+                if solution_four == yy / (f - s):
+                    st.write("(3)②正解")
+                else:
+                    st.write("(3)②不正解")
+            
+            except ValueError:
+                st.write("入力された値が無効です。数字のみを入力してください")
     if serrect == "相対速度":
         st.write("a")
