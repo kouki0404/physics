01da01afd033743ab9f1dd8cec5767adb72de985
kouki0404 <168718919+kouki0404@users.noreply.github.com>
2024-07-26 13:54:17 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,12 +42,12 @@ if chapter == "1章":
     serrect = st.sidebar.selectbox("出題範囲",first)
     if serrect == "合成速度":
         st.session_state.question += 1
-        st.write("速さ" + str(s) + ".0m/sで流れる川の中を、船(静水時での速さ" + str(f) + ".0m/s)が川の流れと平行に進む")
+        st.write("速さ" + str(st.session_state.s) + ".0m/sで流れる川の中を、船(静水時での速さ" + str(st.session_state.f) + ".0m/s)が川の流れと平行に進む")
         st.write("(1) 船が川下に向かって進む場合、川岸で静止している人から見た船の速度は何m/sか。(川下を正とする)")
         solution_one = st.text_area("(1)回答 ※数字のみ")
         st.write("(2) 船が川上に向かって進む場合、川岸で静止している人から見た船の速度は何m/sか。(川下を正とする)")
         solution_two = st.text_area("(2)回答 ※数字のみ")
-        st.write("(3) ①船が川を" + str(xx) + "m下るのと、②" + str(yy) + "m上るのにかかる時間はそれぞれ何秒か。")
+        st.write("(3) ①船が川を" + str(st.session_state.xx) + "m下るのと、②" + str(st.session_state.yy) + "m上るのにかかる時間はそれぞれ何秒か。")
         solution_three = st.text_area("(3)①回答 ※数字のみ")
         solution_four = st.text_area("(3)②回答 ※数字のみ")
         
