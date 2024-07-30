7371032306256ff6a5b29c88003f313f0f441b4a
kouki0404 <168718919+kouki0404@users.noreply.github.com>
2024-07-30 01:45:53 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,9 +53,9 @@ if chapter == "1章":
         st.session_state.question += 1
         st.write("速さ" + str(st.session_state.s) + ".0m/sで流れる川の中を、船(静水時での速さ" + str(st.session_state.f) + ".0m/s)が川の流れと平行に進む")
         st.write("(1) 船が川下に向かって進む場合、川岸で静止している人から見た船の速度は何m/sか。(川下を正とする)")
-        st.session_state.solution_one = st.number_input("(1)回答 ※数字のみ", step=0.1, format="%.1f")
+        st.session_state.solution_one = st.number_input("(1)回答 ※数字のみ", step=1, format="%.1f")
         st.write("(2) 船が川上に向かって進む場合、川岸で静止している人から見た船の速度は何m/sか。(川下を正とする)")
-        st.session_state.solution_two = st.number_input("(2)回答 ※数字のみ", step=0.1, format="%.1f")
+        st.session_state.solution_two = st.number_input("(2)回答 ※数字のみ", step=1, format="%.1f")
         st.write("(3) ①船が川を" + str(xx) + "m下るのと、②" + str(yy) + "m上るのにかかる時間はそれぞれ何秒か。")
         st.session_state.solution_three = st.number_input("(3)①回答 ※数字のみ", step=0.1, format="%.1f")
         st.session_state.solution_four = st.number_input("(3)②回答 ※数字のみ", step=0.1, format="%.1f")
